34da20e97d15acabd0e6db0e5434d991dfc8010e
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-14 16:56:42 -0700

    edited

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = 0;
+	char ch = 'A';
+
+	for (; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	for (' ch < 'F'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+i}
