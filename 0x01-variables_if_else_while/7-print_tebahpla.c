35eda9de9d300015ad64b169242d2f2592623e7e
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-14 16:49:19 -0700

    eighth project

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	for (; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
