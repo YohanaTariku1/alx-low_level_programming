ac3386b695d8436d5c8e79d62f8e4f3bcd7a300b
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-14 17:09:09 -0700

    tenth

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	for (; x < 10; x++)
+	{
+		putchar(x + '0');
+		putchar(',');
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
