0954225aebcdebeda0710394fc24637eb968991b
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-18 16:38:39 -0700

    first

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+/**
+ * print_last_digit - prints last digit of a number
+ * @x: integer input
+ * Return: last value of integer
+ */
+int print_last_digit(int x)
+{
+	int last_digit = x % 10;
+
+	return (last_digit);
+}
