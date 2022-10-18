1a335f68e842ace801aab9234c7498117ee736bd
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-18 15:54:53 -0700

    first

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,14 @@
+/**
+ * print_sign - prints the sign of a number
+ * @n: integer input
+ * Return: 1 for positive, 0 for zero, -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}
