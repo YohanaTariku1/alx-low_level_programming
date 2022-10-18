1e172df2f4e21f42655cfd17b8b15493a4d69fc0
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-18 15:03:15 -0700

    first

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+
+	for (; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	i++;
+	}
+}
