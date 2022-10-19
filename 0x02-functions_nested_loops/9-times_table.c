274f45f54b7e4155dd96a0f639449f92beff9469
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-18 17:10:22 -0700

    first

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints out nine times table
+ * r = row, c = column, d = digits of current result
+ * Return: times table
+ * add extra space
+ */
+void times_table(void)
+{
+	int r, h, d;
+
+	for (r = 0; r < 10; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			d = r * c;
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
