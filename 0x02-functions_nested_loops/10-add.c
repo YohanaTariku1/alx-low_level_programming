e45a02e1d57ea785ee3fd7d5eaf6d42f06c572fe
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-18 17:21:41 -0700

    first

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * add - adds two integers
+ * @x: first input
+ * @y: second input
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	_putchar(z + '0');
+	return (0);
+}
+
