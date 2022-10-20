54a86d5ba644e91c65804decf2902514bd29348e
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-20 14:51:59 -0700

    printed triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,28 @@
 
 void print_triangle(int size)
 {
-	int i, j, k;
+	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= (size - i); j++)
+			for (j = 1; j <= size; j++)
 			{
-				_putchar(' ');
-			}
-			for (k = 1; k <= i; k++)
-			{
-				_putchar('#');
+				if (j <= size - i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
 			}
 			_putchar('\n');
 		}
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar('\n')l
 	}
 }
