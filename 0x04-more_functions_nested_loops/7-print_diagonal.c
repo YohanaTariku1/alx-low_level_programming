7ece57b70b9c4c9f642274a351ce3091b8c2d313
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-20 12:27:01 -0700

    edited agian

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,13 +12,13 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				if (i == j)
 				{
-					_putchar('\');
+					_putchar(92);
 					break;
 				}
 				_putchar(' ');
