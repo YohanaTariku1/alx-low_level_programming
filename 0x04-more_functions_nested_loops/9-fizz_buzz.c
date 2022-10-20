85659000554b4c4970b2ed774dab3954703b37e5
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-20 13:12:18 -0700

    editd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,20 +14,21 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fuzz ");
+			printf("Fuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
