b1daaba037b80a74796859e91e378ddf4b8d4079
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-20 10:43:47 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if input is between 0 and 9
+ * @c: input digit
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (1);
+}
