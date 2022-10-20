6c9b2391bae8612b42e0a21aa400a8ffcda23296
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-20 12:37:11 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints square using #
+ * @size: input integer
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
