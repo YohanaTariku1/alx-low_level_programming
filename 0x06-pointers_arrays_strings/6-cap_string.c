f8c3cacdebcfc81bad41a1385a11e89641727e63
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-27 10:27:03 -0700

    edited

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,57 @@
 #include "main.h"
 
 /**
- * cap_string - a function that capitalizes all words of a string
- * @s: pointer to input string
- * Return: pointer to capitalized string
+ * isLower - determines if char is lowercase
+ * @c: character input
+ * Return: 1 if true, 0 if false
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - determines if char is delimiter
+ * @c: character input
+ * Return: 1 if true, 0 if false
+ */
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: capitalized word
  */
 
 char *cap_string(char *s)
 {
-	int i, j;
-	char sep[] = " \t\n,;.!?\"(){}";
+	char *ptr = s;
+	int foundDelimit = 1;
 
-	i = 1;
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] -= ('a' - 'A');
-	while (s[i] != '\0')
+	while (*s)
 	{
-		for (j = 0; sep[j] != '\0'; j++)
-			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
-				s[i] -= ('a' - 'A');
-		i++
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
 	}
-	return (s);
+	return (ptr);
 }
