a3edf8824f75c52b92da870cb3ef2d10f080ebd5
YohanaTariku1 <yohanatariku1@gmail.com>
2022-10-27 10:36:36 -0700

    leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @c: character input
+ * Return: incoded string
+ */
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + val[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
