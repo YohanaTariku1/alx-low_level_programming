c10287f47b52b460004c552b570c88acb7dc1f12
YohanaTariku1 <yohanatariku1@gmail.com>
2022-11-04 09:56:49 -0700

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints name of program
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
